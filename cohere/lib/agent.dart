19c7f13a1395a613cda72e24bacc0309dbe0731d
Samyak Jain <56000318+samyakkkk@users.noreply.github.com>
2024-05-21 11:07:55 +0530

    (WIP): Cohere agent

---
+++ cohere/lib/agent.dart
@@ -0,0 +1,21 @@
+import 'package:dash_agent/data/datasource.dart';
+import 'package:dash_agent/configuration/command.dart';
+import 'package:dash_agent/configuration/dash_agent.dart';
+import 'commands/ask.dart';
+import 'data_sources.dart';
+
+/// [MyAgent] consists of all your agent configuration.
+///
+/// This includes:
+/// [DataSource] - For providing additional data to commands to process.
+/// [Command] - Actions available to the user in the IDE, like "/ask", "/generate" etc
+class MyAgent extends AgentConfiguration {
+  final docsSource = DocsDataSource();
+
+  @override
+  List<DataSource> get registeredDataSources => [docsSource];
+
+  @override
+  List<Command> get registerSupportedCommands =>
+      [AskCommand(docsSource: docsSource)];
+}
