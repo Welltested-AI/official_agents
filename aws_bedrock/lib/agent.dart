32ecfba889fea396fd3b1487f7f49ec66834e685
Samyak Jain <56000318+samyakkkk@users.noreply.github.com>
2024-06-03 19:51:23 +0530

    (agent): Bedrock

---
+++ aws_bedrock/lib/agent.dart
@@ -0,0 +1,36 @@
+import 'package:dash_agent/configuration/metadata.dart';
+import 'package:dash_agent/data/datasource.dart';
+import 'package:dash_agent/configuration/command.dart';
+import 'package:dash_agent/configuration/dash_agent.dart';
+import 'commands/ask.dart';
+import 'data_sources.dart';
+
+/// [MyAgent] consists of all your agent configuration.
+///
+/// This includes:
+/// [DataSource] - For providing additional data to commands to process.
+/// [Command] - Actions available to the user in the IDE, like "/ask", "/generate" etc
+class MyAgent extends AgentConfiguration {
+  final docsDataSource = DocsDataSource();
+
+  @override
+  Metadata get metadata => Metadata(
+      name: 'AWS Bedrock',
+      avatarProfile: 'assets/logo.png',
+      tags: ['Generative AI']);
+
+  @override
+  String get registerSystemPrompt => '''You are an Amazon Bedrock assistant. 
+      
+      Amazon Bedrock is a fully managed service that offers a choice of high-performing foundation models (FMs) from leading AI companies like AI21 Labs, Anthropic, Cohere, Meta, Mistral AI, Stability AI, and Amazon through a single API, along with a broad set of capabilities you need to build generative AI applications.
+      
+      Help users with their queries by answering from the latest documentation that is provided to you. Be truthful and don't hestiate to tell you do not know if the documents shared do not answer user's query.''';
+
+  @override
+  List<DataSource> get registerDataSources => [docsDataSource];
+
+  @override
+  List<Command> get registerSupportedCommands => [
+        // AskCommand(docsSource: docsDataSource)
+      ];
+}
