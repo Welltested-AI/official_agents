10b82320ac1b280c91a0c94c095dd284fcca3422
Yogesh Choudhary <youzendachoudhary22@gmail.com>
2024-06-07 14:17:13 +0530

    go_router update: enable commendless mode

--- go_router/lib/agent.dart
+++ go_router/lib/agent.dart
@@ -1,3 +1,4 @@
+import 'package:dash_agent/configuration/metadata.dart';
 import 'package:dash_agent/data/datasource.dart';
 import 'package:dash_agent/configuration/command.dart';
 import 'package:dash_agent/configuration/dash_agent.dart';
@@ -18,7 +19,7 @@ class GoRouterAgent extends AgentConfiguration {
       : issueSource = IssuesDataSource(issuesLinks);
 
   @override
-  List<DataSource> get registeredDataSources =>
+  List<DataSource> get registerDataSources =>
       [docsSource, exampleSource, issueSource, testExampleSource];
 
   @override
@@ -35,4 +36,15 @@ class GoRouterAgent extends AgentConfiguration {
             issueDataSource: issueSource),
         TestCommand(testExampleSource)
       ];
+
+  @override
+  Metadata get metadata => Metadata(
+      name: 'GoRouter',
+      tags: ['flutter', 'dart', 'flutter package', 'flutter favorite']);
+
+  @override
+  String get registerSystemPrompt =>
+      '''You are a GoRouter Integration assistant inside user's IDE. GoRouter is a declarative routing package for Flutter that uses the Router API to provide a convenient, url-based API for navigating between different screens.
+      
+      You will be provided with latest docs and examples relevant to user queries and you have to help user with any questions they have related to GoRouter. Output code and code links wherever required and answer "I don't know" if the user query is not covered in the docs provided to you''';
 }
