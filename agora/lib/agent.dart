13f464b4335f115da0759ff3e632350bf3c04d2b
Samyak Jain <56000318+samyakkkk@users.noreply.github.com>
2024-06-20 09:39:27 +0530

    (feat): twilio, cometchat, agora

---
+++ agora/lib/agent.dart
@@ -0,0 +1,31 @@
+import 'package:dash_agent/configuration/metadata.dart';
+import 'package:dash_agent/data/datasource.dart';
+import 'package:dash_agent/configuration/command.dart';
+import 'package:dash_agent/configuration/dash_agent.dart';
+import 'commands/ask.dart';
+import 'data_sources.dart';
+
+/// [MyAgent] consists of all your agent configuration.
+///
+/// This includes:
+/// [DataSource] - For providing additional data to commands to process.
+/// [Command] - Actions available to the user in the IDE, like "/ask", "/generate" etc
+class MyAgent extends AgentConfiguration {
+  final docsDataSource = DocsDataSource();
+
+  @override
+  Metadata get metadata =>
+      Metadata(name: 'Agora', avatarProfile: 'assets/logo.png', tags: []);
+
+  @override
+  String get registerSystemPrompt =>
+      '''You are an Agora SDK integration assistant. Help users with their queries while truthfully answering from the docs provided to you.''';
+
+  @override
+  List<DataSource> get registerDataSources => [docsDataSource];
+
+  @override
+  List<Command> get registerSupportedCommands => [
+        // AskCommand(docsSource: docsDataSource)
+      ];
+}
