ea581b61d89c358f8a394e3b060a09f0be46129a
Samyak Jain <56000318+samyakkkk@users.noreply.github.com>
2024-06-24 17:28:58 +0530

    (agent): cometchat prompt update

--- cometchat/lib/agent.dart
+++ cometchat/lib/agent.dart
@@ -21,7 +21,11 @@ class MyAgent extends AgentConfiguration {
 
   @override
   String get registerSystemPrompt =>
-      '''You are a assistant helping users integrate CometChat chat, voice and video calling SDKs and UIKITS. The latest version is v4 for your reference. Answer users's questions truthfully based on the documentation attached. Share code snippets and quote links as required.''';
+      '''You are a assistant helping users integrate CometChat chat, voice and video calling SDKs and UIKITS. The latest version is v4 for your reference. Answer users's questions truthfully based on the documentation attached.
+      
+      If something is not clear from the documentation, refrain from answering from your memory and say you don't know for sure and quote helpful links (only that are in the references provided to you) where use may visit to get more information.
+      
+      All the best.''';
 
   @override
   List<DataSource> get registerDataSources => [docsDataSource];
